98145f5011cd3f1dea9da5dc96eb2abaf7d9819d
rsadek <rita.sadek@cern.ch>
2021-06-21 18:21:00 +0200

    Some fixes added to the code, first compilable version

--- Detectors/Vertexing/include/DetectorsVertexing/FwdDCAFitterN.h
+++ Detectors/Vertexing/include/DetectorsVertexing/FwdDCAFitterN.h
@@ -854,11 +854,12 @@ void FwdDCAFitterN<N, Args...>::findZatXY_mid(int mCurHyp) // Between 2 tracks
     while (DeltaZ > epsilon){
 
       midPoint=0.5*(startPoint+endPoint);
-      
+
+      trc = trc0;
       for (int i=0; i<2; i++){
-        trc[i].propagateToZlinear(startPoint);
-        newX[i][0] = trc[i].getX();
-        newY[i][0] = trc[i].getY();
+        trc.propagateToZlinear(startPoint);
+        newX[i][0] = trc.getX();
+        newY[i][0] = trc.getY();
 
       for (int i=0; i<2; i++){
         mCandTr[mCurHyp][i].propagateParamToZlinear(startPoint);
